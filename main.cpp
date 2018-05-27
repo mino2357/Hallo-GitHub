3d9403dae2d716448cffdcf21e66cc16c911fb1e
mino2357 <mino2357@gmail.com>
2018-05-27 20:47:25 +0900

    足し算をする函数を実装
    足し算函数addを実装した．コンパイル，動作ともに確認した．

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+//足し算函数add．
+template <typename T = double>
+T add(T&& x, T&& y){
+  return x + y;
+}
+
 int main(){
   std::cout << "Hello World!" << std::endl;
   std::cout << "2 + 3" << std::endl;
   std::cout << 2 + 3 << std::endl;
+  std::cout << add(2, 3) << std::endl;
 }
